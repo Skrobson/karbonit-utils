8c416d951b0ce97d00db5d6ffbe41114810cb8e5
Skrobson <skrobolbartlomiej@gmail.com>
2022-12-15 19:41:45 +0100

    Generate version from git tag

---
+++ CMake/versioning/GenerateVersionHeader.cmake
@@ -0,0 +1,32 @@
+function(generateVersionFromGit)
+if(GIT_EXECUTABLE)
+  get_filename_component(SRC_DIR ${SRC} DIRECTORY)
+  # Generate a git-describe version string from Git repository tags
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --tags --long --dirty --match "v*"
+    WORKING_DIRECTORY ${SRC_DIR}
+    OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
+    RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  if(NOT GIT_DESCRIBE_ERROR_CODE)
+    set(VERSION_STRING ${GIT_DESCRIBE_VERSION})
+   
+  string(REGEX REPLACE "^v([0-9]+)\\..*" "\\1" VERSION_MAJOR "${VERSION_STRING}")
+  string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION_STRING}")
+  string(REGEX REPLACE "^v[0-9]+\\.[0-9]+-([0-9]+).*" "\\1" VERSION_PATCH "${VERSION_STRING}")
+  string(REGEX REPLACE "^v[0-9]+\\.[0-9]+-[0-9]+-(.*)" "\\1" VERSION_SHA1 "${VERSION_STRING}")
+  
+  endif()
+endif()
+
+# Final fallback: Just use a bogus version string that is semantically older
+# than anything else and spit out a warning to the developer.
+if(NOT DEFINED VERSION_STRING)
+  set(VERSION_STRING v0.0.0-unknown)
+  message(WARNING "Failed to determine VERSION_STRING from Git tags. Using default version \"${VERSION_STRING}\".")
+endif()
+
+configure_file(${SRC} ${DST} @ONLY)
+configure_file(${CMAKE_SRC} ${CMAKE_DST} @ONLY)
+endfunction()
