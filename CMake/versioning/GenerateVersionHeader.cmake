f040ee9277885e2d1bbcab7f37907de4a83f597d
Skrobson <skrobolbartlomiej@gmail.com>
2023-01-07 18:02:38 +0100

    refactor

--- CMake/versioning/GenerateVersionHeader.cmake
+++ CMake/versioning/GenerateVersionHeader.cmake
@@ -1,16 +1,20 @@
+find_package(Git REQUIRED)
+
+
 set(HEADER_SRC ${CMAKE_CURRENT_LIST_DIR}/version.h.in)
-set(HEADER_DST ${CMAKE_BINARY_DIR}/utils/CMake/versioning/version.h)
-
 set(CMAKE_SRC ${CMAKE_CURRENT_LIST_DIR}/VersionForPackage.cmake.in)
-set(CMAKE_DST ${CMAKE_BINARY_DIR}/utils/CMake/versioning/VersionForPackage.cmake)
 
-function(generateVersionFromGit)
+function(generateVersionFromGit version_lib_name src_directory)
+
+set(HEADER_DST ${CMAKE_BINARY_DIR}/utils/CMake/versioning/include/${version_lib_name}.h)
+set(CMAKE_DST ${CMAKE_BINARY_DIR}/utils/CMake/versioning/${version_lib_name}ForPackage.cmake)
+
 if(GIT_EXECUTABLE)
-  message(STATUS "GenerateVersionHeader for: ${DIRECTORY}")
+  message(STATUS "GenerateVersion ${version_lib_name} for ${src_directory} directory.")
   # Generate a git-describe version string from Git repository tags
   execute_process(
     COMMAND ${GIT_EXECUTABLE} describe --tags --long --dirty --match "v*"
-    WORKING_DIRECTORY ${DIRECTORY}
+    WORKING_DIRECTORY ${src_directory}
     OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
     RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -22,7 +26,7 @@ if(GIT_EXECUTABLE)
   string(REGEX REPLACE "^v.?[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION_STRING}")
   string(REGEX REPLACE "^v.?[0-9]+\\.[0-9]+-([0-9]+).*" "\\1" VERSION_PATCH "${VERSION_STRING}")
   string(REGEX REPLACE "^v.?[0-9]+\\.[0-9]+-[0-9]+-(.*)" "\\1" VERSION_SHA1 "${VERSION_STRING}")
-  
+  set(VER_LIB ${version_lib_name})
   endif()
 endif()
 
@@ -35,4 +39,10 @@ endif()
 
 configure_file(${HEADER_SRC} ${HEADER_DST} @ONLY)
 configure_file(${CMAKE_SRC} ${CMAKE_DST} @ONLY)
+
+add_library(${version_lib_name} INTERFACE)
+target_include_directories(${version_lib_name} INTERFACE ${CMAKE_BINARY_DIR}/utils/CMake/versioning/include)
+
+include(${CMAKE_BINARY_DIR}/utils/CMake/versioning/${version_lib_name}ForPackage.cmake)
+
 endfunction()
