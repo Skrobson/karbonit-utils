e015f78d30d98f15348f71d2388760a63a703ddc
Skrobson <skrobolbartlomiej@gmail.com>
2024-05-22 19:38:03 +0200

    add dependencies to script

--- dependencies.sh
+++ dependencies.sh
@@ -4,6 +4,8 @@ sudo apt update
 sudo apt install cmake \
     git \
     -y
+sudo apt install libspdlog-dev -y
+sudo apt install libboost1.83-all-dev
 
 install sudo apt install llvm clang-format clang-tidy uncrustify cppcheck iwyu -y
 
